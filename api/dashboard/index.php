54416c33952e4540fe0935110f29ed7072b0c389
Mafalda Brito <81653648+mafaldabbrito@users.noreply.github.com>
2025-08-11 16:26:32 +0000

    deleting not needed files

--- api/dashboard/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$title = 'Dashboard - EOW PWA Demo';
-$manifest = '/manifests/manifest-dashboard.json';
-$icon = '/icons/dashboard.svg';
-$themeColor = '#6b7280';
-$contentFile = __DIR__ . '/../views/dashboard.php';
-
-include __DIR__ . '/../layout.php';
--- api/settings/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$title = 'Settings - EOW PWA Demo';
-$manifest = '/manifests/manifest-settings.json';
-$icon = '/icons/settings.png';
-$themeColor = '#8b4513';
-$contentFile = __DIR__ . '/../views/settings.php';
-
-include __DIR__ . '/../layout.php';
